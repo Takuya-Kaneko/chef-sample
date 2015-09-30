5f72334044ca968efeebaaefee348546d2f5f9e6
Takuya Kaneko <chinju->
2015-10-01 00:27:06 +0900

    add mysql recipe

---
+++ site-cookbooks/mysql/attributes/default.rb
@@ -0,0 +1,6 @@
+default['mysql']['server_root_password'] = 'passw0rd'
+default['mysql']['admin']['user'] = 'admin'
+default['mysql']['admin']['password'] = 'passw0rd'
+default['mysql']['database'] = 'database'
+
+default['mysql-community-server']['version'] = '5.6.26-2.el6'
